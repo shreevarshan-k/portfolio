2f6ebb1ff6ca6e5891c03663140cbda485432d95
shreevarshan-k <shreevarshan.k.2018.it@rajalakshmi.edu.in>
2021-04-12 14:36:57 +0530

    Updates

---
+++ precache-manifest.a69ef776d0c2bfdfc8fee83825af3fc7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d880cb94af4949cf2d9a",
+    "url": "/portfolio/static/css/main.1fc72914.chunk.css"
+  },
+  {
+    "revision": "d880cb94af4949cf2d9a",
+    "url": "/portfolio/static/js/main.d880cb94.chunk.js"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/portfolio/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "8f2bea64fdad47806e4148ccb37f4c6b",
+    "url": "/portfolio/index.html"
+  }
+];
